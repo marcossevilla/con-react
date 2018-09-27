a4ff105c29d1ad4b0a9b343907903e8422370b09
Marcos Sevilla <marcossevilla14@gmail.com>
2018-09-27 22:54:02 +0000

    single page part 1

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,74 +1,74 @@
 import React from 'react';
 import { Card, CardImg, CardTitle, CardText, CardBody } from 'reactstrap';
 
-    function RenderDish({dish}) {
-        if (dish != null) {
-            return(
-                <div className="col-12 col-md-5 m-1">
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-                </div>
-            );
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }    
+function RenderDish({dish}) {
+    if (dish != null) {
+        return(
+            <div className="col-12 col-md-5 m-1">
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+            </div>
+        );
     }
+    else {
+        return (
+            <div></div>
+        );
+    }    
+}
 
     
-    function RenderComments({comments}) {
-        
-        if (comments != null) {
-            const commentList = comments.map((comment) => { 
-                return (
-                    <ul className='list-unstyled'>
-                        <li key={comment.id}>
-                            <p> {comment.comment} </p>
-                            <p>-- {comment.author} , 
-                                &nbsp; {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                            </p>
-                        </li>
-                    </ul>   
-                );
-            });
-                
+function RenderComments({comments}) {
+    
+    if (comments != null) {
+        const commentList = comments.map((comment) => { 
             return (
-                <div className="col-12 col-md-5 m-1">
-                <h4> Comments </h4>
-                    {commentList}
-                </div>
-            )
-        }
-        else {
-            return (
-                <div></div>
+                <ul className='list-unstyled'>
+                    <li key={comment.id}>
+                        <p> {comment.comment} </p>
+                        <p>-- {comment.author} , 
+                            &nbsp; {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                        </p>
+                    </li>
+                </ul>   
             );
-        }
+        });
+            
+        return (
+            <div className="col-12 col-md-5 m-1">
+            <h4> Comments </h4>
+                {commentList}
+            </div>
+        )
     }
-        
-    const Dishdetail = (props) => {
-        if (props.dish != null) {
-            return ( 
-                <div class="container">
-                    <div className="row">
-                        <RenderDish dish={props.dish}/>
-                        <RenderComments comments={props.dish.comments}/>
-                    </div>
-                </div>
-            );
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
+    else {
+        return (
+            <div></div>
+        );
     }
+}
+    
+const Dishdetail = (props) => {
+    if (props.dish != null) {
+        return ( 
+            <div class="container">
+                <div className="row">
+                    <RenderDish dish={props.dish}/>
+                    <RenderComments comments={props.dish.comments}/>
+                </div>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div></div>
+        );
+    }
+}
 
 export default Dishdetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 // eslint-disable-next-line
 import DishDetail from './DishdetailComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -13,7 +17,10 @@ class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            dishes: DISHES
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         };
     }
 
@@ -21,7 +28,11 @@ class Main extends Component {
         
         const HomePage = () => {
             return (
-                <Home />    
+                <Home 
+                    dish = {this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion = {this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader = {this.state.leaders.filter((leader) => leader.featured)[0]}
+                />    
             );
         } 
       
@@ -31,6 +42,7 @@ class Main extends Component {
             <Switch>
                 <Route path="/home" component={HomePage} />
                 <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes}/> } />
+                <Route exact path="/contactus" component={Contact} />
                 <Redirect to="/home" />
             </Switch>
             <Footer />
