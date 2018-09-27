ecde0daf2f756304b7b132fb6a9d947a287bdb11
Marcos Sevilla <marcossevilla14@gmail.com>
2018-09-27 20:37:06 +0000

    react router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,34 +1,42 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Menu from './MenuComponent';
+// eslint-disable-next-line
 import DishDetail from './DishdetailComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        dishes: DISHES,
-        selectedDish: null
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-        <Footer />
-      </div>
-    );
-  }
+    render() {
+        
+        const HomePage = () => {
+            return (
+                <Home />    
+            );
+        } 
+      
+        return (
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/home" component={HomePage} />
+                <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes}/> } />
+                <Redirect to="/home" />
+            </Switch>
+            <Footer />
+        </div>
+        );
+    }
 }
 
 export default Main;
