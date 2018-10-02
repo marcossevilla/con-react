90cb347dd3aa835338a038a5fc615cc2cebaf80e
Marcos Sevilla <marcossevilla14@gmail.com>
2018-10-02 05:10:07 +0000

    controlled forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
@@ -50,6 +51,7 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path="/home" component={HomePage} />
+                    <Route path="/aboutus" component={ () => <About leaders={this.state.leaders}/> } />
                     <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes}/> } />
                     <Route path="/menu/:dishId" component={DishWithId}/>
                     <Route exact path="/contactus" component={Contact} />
